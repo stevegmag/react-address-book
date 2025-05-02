e156562e24450b99266d4e302dc1725dd803c6e6
Steven Gallagher <stevegmag@gmail.com>
2025-05-02 18:00:37 -0400

    annimations to open and change details

--- client-react/src/components/Contact.js
+++ client-react/src/components/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ContactDetails from './ContactDetails';
 import EditContactDetails from './EditContactDetails';
 
@@ -6,6 +6,17 @@ const Contact = ({contact, activeContact, setActiveContact, isEditing, setIsEdit
   const {id, firstName, lastName, email} = contact;
   const firstInit = firstName.charAt(0);
   const isActive = activeContact === id;
+  const [showEditMode, setShowEditMode] = useState(isEditing);
+  const containerRef = useRef(null);
+  
+  // Update showEditMode when isEditing changes
+  useEffect(() => {
+    // Small delay to allow animation to complete
+    const timer = setTimeout(() => {
+      setShowEditMode(isEditing);
+    }, 50);
+    return () => clearTimeout(timer);
+  }, [isEditing]);
   
   // Generate a random pastel color for the background
   const getRandomColor = () => {
@@ -19,12 +30,35 @@ const Contact = ({contact, activeContact, setActiveContact, isEditing, setIsEdit
 
   const handleClick = () => {
     isActive ? setActiveContact(null) : setActiveContact(id);
-    isEditing ? setIsEditing(false) : null;
+    if (isEditing) setIsEditing(false);
+  };
+
+  // Render the appropriate component based on edit mode
+  const renderContent = () => {
+    if (showEditMode) {
+      return (
+        <EditContactDetails 
+          contact={contact} 
+          activeContact={activeContact}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+        />
+      );
+    } else {
+      return (
+        <ContactDetails 
+          contact={contact} 
+          activeContact={activeContact}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+        />
+      );
+    }
   };
 
   return(
     <>
-      <li className="contact-list--item" key={id} onClick={handleClick}>
+      <li className="contact-list--item" onClick={handleClick}>
         <span 
           className="contact-list--initial" 
           style={{ backgroundColor: bgColor }}
@@ -34,25 +68,17 @@ const Contact = ({contact, activeContact, setActiveContact, isEditing, setIsEdit
         <div>
           <span className="contact-list--name">{firstName} {lastName}</span>
           <br />{email}
-          {isEditing ? (
-            <EditContactDetails 
-              contact={contact} 
-              activeContact={activeContact}
-              isEditing={isEditing}
-              setIsEditing={setIsEditing}
-            />
-          ) : (
-            <ContactDetails 
-              contact={contact} 
-              activeContact={activeContact}
-              isEditing={isEditing}
-              setIsEditing={setIsEditing}
-            />
-          )}
+          
+          <div 
+            className={`slide-container ${isActive ? 'active' : ''}`}
+            ref={containerRef}
+          >
+            {isActive && renderContent()}
+          </div>
         </div>
         {/* Arrow indicator */}
         <span className={`contact-list--arrow ${isActive ? 'active' : ''}`}>
-          &#9654; {/* Unicode right-pointing triangle */}
+          &#9654;
         </span>
       </li>
     </>
